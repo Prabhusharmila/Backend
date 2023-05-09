dcdca4c75e782c62cde4dfcfa5dcb12e40bf5587
Prabhusharmila <prabhusharmila.t@gmail.com>
2023-05-09 19:48:48 -0400

    Backend

--- app.js
+++ app.js
@@ -1,62 +1,94 @@
-let express = require('express'),
-  cors = require('cors'),
-  mongoose = require('mongoose'),
-  mongo=require('mongodb'),
-  database = require('./database'),
-  bodyParser = require('body-parser');
+const http = require('http');
+const url = require('url');
+const MongoClient = require('mongodb').MongoClient;
+const database = require('./database');
 
-  const createError = require('http-errors');
+// Connect to MongoDB
+MongoClient.connect(database.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, function(err, client) {
+  if (err) {
+    console.log("Database couldn't be connected to: " + err);
+  } else {
+    console.log("Database connected");
+    
+    // Create server
+    const server = http.createServer((req, res) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, UPDATE, PUT, PATCH');
+      res.setHeader('Content-Type', 'application/json');
 
-// Connect mongoDB
-mongoose.Promise = global.Promise;
-mongoose.connect(database.db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Database connected")
-  },
-  error => {
-    console.log("Database couldn't be connected to: " + error)
+      // Handle requests
+      const reqUrl = url.parse(req.url, true);
+      const pathname = reqUrl.pathname;
+      const query = reqUrl.query;
+      const method = req.method;
+
+      // Handle API requests
+      if (pathname === '/api/students') {
+        const collection = client.db(database.dbName).collection(database.collections.students);
+
+        if (method === 'GET') {
+          // Get all students
+          collection.find({}).toArray(function(err, data) {
+            if (err) {
+              res.statusCode = 500;
+              res.end(JSON.stringify({ message: "Error fetching students" }));
+            } else {
+              res.statusCode = 200;
+              res.end(JSON.stringify(data));
+            }
+          });
+        } else if (method === 'POST') {
+          // Add new student
+          const student = query;
+          collection.insertOne(student, function(err, result) {
+            if (err) {
+              res.statusCode = 500;
+              res.end(JSON.stringify({ message: "Error adding student" }));
+            } else {
+              res.statusCode = 201;
+              res.end(JSON.stringify({ message: "Student added successfully" }));
+            }
+          });
+        } else if (method === 'PUT') {
+          // Update student
+          const studentId = query.id;
+          const newValues = { $set: query };
+          collection.updateOne({ _id: new MongoClient.ObjectId(studentId) }, newValues, function(err, result) {
+            if (err) {
+              res.statusCode = 500;
+              res.end(JSON.stringify({ message: "Error updating student" }));
+            } else {
+              res.statusCode = 200;
+              res.end(JSON.stringify({ message: "Student updated successfully" }));
+            }
+          });
+        } else if (method === 'DELETE') {
+          // Delete student
+          const studentId = query.id;
+          collection.deleteOne({ _id: new MongoClient.ObjectId(studentId) }, function(err, result) {
+            if (err) {
+              res.statusCode = 500;
+              res.end(JSON.stringify({ message: "Error deleting student" }));
+            } else {
+              res.statusCode = 200;
+              res.end(JSON.stringify({ message: "Student deleted successfully" }));
+            }
+          });
+        }
+      } else {
+        // Handle 404 error
+        res.statusCode = 404;
+        res.end(JSON.stringify({ message: "Page not found" }));
+      }
+    });
+
+    // Listen on port
+    const port = process.env.PORT || 5000;
+    server.listen(port, () => {
+      console.log('Server running on port ' + port);
+    });
   }
-)
-
-const studentAPI = require('./routes/student.route')
-const header = {
-  origin:'*'
-};
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cors(header));
-app.use(cors({
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
-
-// API
-app.use('/api', studentAPI)
-
-// Create port
-const port = process.env.PORT || 5000;
-const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
-
-app.use(express.static('public'));
-
- app.get('/',function(req,res) {
-     res.sendFile(__dirname + '/public/index.html');
-   });
-//Find 404
-
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  console.error(err.message);
-  if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
 });
--- routes/student.route.js
+++ routes/student.route.js
@@ -1,64 +1,69 @@
-const express = require('express');
-const studentRoute = express.Router();
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const StudentModel = require('../models/Student');
 
-// Student model
-let StudentModel = require('../models/Student');
+const studentRoute = (app) => {
 
-studentRoute.route('/').get((req, res) => {
-    StudentModel.find((error, data) => {
-     if (error) {
-       return next(error)
-     } else {
-       res.json(data)
-     }
-   })
- })
+app.get('/students', (req, res, next) => {
+StudentModel.find((error, data) => {
+if (error) {
+return next(error)
+} else {
+res.json(data)
+}
+})
+})
 
- studentRoute.route('/create-student').post((req, res, next) => {
-    StudentModel.create(req.body, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
+app.post('/create-student', (req, res, next) => {
+StudentModel.create(req.body, (error, data) => {
+if (error) {
+return next(error)
+} else {
+res.json(data)
+}
+})
 });
 
-studentRoute.route('/edit-student/:id').get((req, res) => {
-   StudentModel.findById(req.params.id, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
+app.get('/edit-student/:id', (req, res, next) => {
+StudentModel.findById(req.params.id, (error, data) => {
+if (error) {
+return next(error)
+} else {
+res.json(data)
+}
+})
 })
 
 // Update student
-studentRoute.route('/update-student/:id').post((req, res, next) => {
-  StudentModel.findByIdAndUpdate(req.params.id, {
-    $set: req.body
-  }, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.json(data)
-      console.log('Student successfully updated!')
-    }
-  })
+app.post('/update-student/:id', (req, res, next) => {
+StudentModel.findByIdAndUpdate(req.params.id, {
+$set: req.body
+}, (error, data) => {
+if (error) {
+return next(error);
+} else {
+res.json(data)
+console.log('Student successfully updated!')
+}
+})
 })
 
 // Delete student
-studentRoute.route('/delete-student/:id').delete((req, res, next) => {
-  StudentModel.findByIdAndRemove(req.params.id, (error, data) => {
-    if (error) {
-      return next(error);
-    } else {
-      res.status(200).json({
-        msg: data
-      })
-    }
-  })
+app.delete('/delete-student/:id', (req, res, next) => {
+StudentModel.findByIdAndRemove(req.params.id, (error, data) => {
+if (error) {
+return next(error);
+} else {
+res.status(200).json({
+msg: data
+})
+}
+})
 })
 
+}
+
 module.exports = studentRoute;
+
+
